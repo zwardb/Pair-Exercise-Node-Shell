69c6ed1e92153f6446a74641ac161df05dc65848
Bradley N. Plunkett <bradleyn.plunkett@bradleys-mbp.myfiosgateway.com>
2021-09-24 14:34:23 -0400

    switching driver and navigator

---
+++ bash.js
@@ -0,0 +1,13 @@
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim();
+
+  if (cmd === 'pwd'){
+    process.stdout.write(process.cwd())
+  }
+
+  else {
+  process.stdout.write('You typed!!!: ' + cmd);
+  process.stdout.write('\nprompt > ');
+}})
